395177c6c118735d33b2dd45ca8b3b8fe5661878
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-05-23 21:45:48 +0100

    add app layout

--- app/src/main/java/com/codeclan/code/example/wordcounter/MainActivity.java
+++ app/src/main/java/com/codeclan/code/example/wordcounter/MainActivity.java
@@ -2,8 +2,12 @@ package com.codeclan.code.example.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
