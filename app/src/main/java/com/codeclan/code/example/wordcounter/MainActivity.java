6646991af98815d6c7463fa1a1620e98d108271c
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-05-23 21:51:54 +0100

    add properties to MainActivity

--- app/src/main/java/com/codeclan/code/example/wordcounter/MainActivity.java
+++ app/src/main/java/com/codeclan/code/example/wordcounter/MainActivity.java
@@ -7,11 +7,20 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private EditText inputTextBox;
+    private Button countWordsButton;
+    private EditText displayWordsCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputTextBox = (EditText) findViewById(R.id.input_text_box);
+        countWordsButton = (Button) findViewById(R.id.button_count_words);
+        displayWordsCount = (EditText) findViewById(R.id.display_words_count);
     }
+
+
+
 }
